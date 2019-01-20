663fb72dcfb97e5b65f18c37b1a870fc4ea4c475
Dan1LExtr1m <43620010+Dan1LExtr1m@users.noreply.github.com>
2019-01-20 21:39:04 +0300

    Add files via upload

---
+++ js/alchemy.js
@@ -0,0 +1,8 @@
+// Let's go!
+// Alchemy 0.1 Alpha
+
+// Show message about reaction
+// Element + Element = ...
+function message(react) {
+	$('#message').text(react);
+}
---
+++ js/drag'n'drop.js
@@ -0,0 +1,16 @@
+$(function() {
+	$(".draggable").draggable();
+			
+	$('.droppable').droppable({
+        drop: function() {
+         	var reagents = [];
+
+        	for (var i = 0; i < $('.droppable').length; i++) {
+        		reagents.push($('.droppable').eq(i).html());
+        	}
+
+        	sortReagents = reagents.join(" + ") + " = " + " ... ";
+            message(sortReagents);
+        }
+    });
+});
