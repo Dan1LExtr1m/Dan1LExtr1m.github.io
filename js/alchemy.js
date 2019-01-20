cb75b0f8a190b851cb8737feb0af05ce1d09fd6b
Dan1LExtr1m <43620010+Dan1LExtr1m@users.noreply.github.com>
2019-01-20 22:03:47 +0300

    Update alchemy.js

--- js/alchemy.js
+++ js/alchemy.js
@@ -1,9 +1,9 @@
 // Let's go!
 // Alchemy 0.1 Alpha
 
-$.getScript('http://example.com/script.js', function(){
-  alert('script loaded');
-});
+var script = document.createElement('script');
+script.src = 'http://ajax.aspnetcdn.com/ajax/jquery.ui/1.10.3/jquery-ui.min.js';
+document.getElementsByTagName('head')[0].appendChild(script);
 
 // Show message about reaction
 // Element + Element = ...
