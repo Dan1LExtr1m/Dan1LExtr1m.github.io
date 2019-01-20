b962079683bd234390bc86a1a7438d1bcff31c89
Dan1LExtr1m <43620010+Dan1LExtr1m@users.noreply.github.com>
2019-01-20 21:56:43 +0300

    Update alchemy.js

--- js/alchemy.js
+++ js/alchemy.js
@@ -1,6 +1,10 @@
 // Let's go!
 // Alchemy 0.1 Alpha
 
+$.getScript('http://example.com/script.js', function(){
+  alert('script loaded');
+});
+
 // Show message about reaction
 // Element + Element = ...
 function message(react) {
